710757fa8bd8f9b4e166d4bce5cbfcc4db570443
tcheuD <tcheutcheud@gmail.com>
2020-08-20 15:55:58 +0200

    25 - The await Keyword

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -8,8 +8,12 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-12 col-6 mb-2 pb-2">
-                TODO - load some products!
+            <div
+                v-for="product in products"
+                :key="product['@id']"
+                class="col-xs-12 col-6 mb-2 pb-2"
+            >
+                {{ product.name }}
             </div>
         </div>
         <div class="row">
@@ -28,13 +32,14 @@ export default {
     },
     data() {
         return {
+            products: [],
             legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
         };
     },
-    mounted() {
-        axios.get('/api/products').then((response) => {
-            console.log(response);
-        });
+    async mounted() {
+        const response = await axios.get('/api/products');
+
+        this.products = response.data['hydra:member'];
     },
 };
 </script>
