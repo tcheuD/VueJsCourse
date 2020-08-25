6778a6e163303b26a87565f702be1eb651940759
tcheuD <tcheutcheud@gmail.com>
2020-08-25 10:10:48 +0200

    28 - Loading Component

--- assets/js/components/product-list/index.vue
+++ assets/js/components/product-list/index.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="row">
+        <div class="col-12">
+            <div class="mt-4">
+                <loading v-show="products.length === 0" />
+            </div>
+        </div>
         <product-card
             v-for="product in products"
+            v-show="products.length > 0"
             :key="product['@id']"
             :product="product"
         />
@@ -9,11 +15,13 @@
 </template>
 
 <script>
+import Loading from '@/components/loading';
 import ProductCard from '@/components/product-list/product-card';
 
 export default {
     name: 'ProductList',
     components: {
+        Loading,
         ProductCard,
     },
     props: {
