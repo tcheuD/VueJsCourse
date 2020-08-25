fe2075b4ba2e0eef899a933784e5f63836fa7cc5
tcheuD <tcheutcheud@gmail.com>
2020-08-25 14:44:25 +0200

    33 - Filtering the Products

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -26,6 +26,12 @@ export default {
         LegendComponent,
         ProductList,
     },
+    props: {
+        currentCategoryId: {
+            type: String,
+            default: null,
+        },
+    },
     data() {
         return {
             products: [],
@@ -33,7 +39,14 @@ export default {
         };
     },
     async created() {
-        const response = await axios.get('/api/products');
+        const params = {};
+        if (this.currentCategoryId) {
+            params.category = this.currentCategoryId;
+        }
+
+        const response = await axios.get('/api/products', {
+            params,
+        });
 
         this.products = response.data['hydra:member'];
     },
