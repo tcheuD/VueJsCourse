0beb22e686a5158fcc6c25d95a007edc8a960b2a
tcheuD <tcheutcheud@gmail.com>
2020-09-03 12:54:15 +0200

    45 - Business Logic Helpers

--- assets/js/components/product-list/product-card.vue
+++ assets/js/components/product-list/product-card.vue
@@ -30,6 +30,8 @@
 </template>
 
 <script>
+import formatPrice from '@/helpers/format-price';
+
 export default {
     name: 'ProductCard',
     props: {
@@ -44,8 +46,7 @@ export default {
      * @returns {string}
      */
         price() {
-            return (this.product.price / 100)
-                .toLocaleString('en-US', { minimumFractionDigits: 2 });
+            return formatPrice(this.product.price);
         },
     },
 };
